7d7fd90cb52556a38e9ce8c8efae5e2eff0e1363
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2023-09-23 17:42:49 +0400

    literature-quiz

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,15 +1,38 @@
 import styled from "styled-components";
 import windows11 from "../assets/windows11.jpeg";
+import error from "../assets/error.png";
+import errorSound from "../assets/erro.mp3";
+import { useState } from "react";
 
 const Start = () => {
+  const [error, setError] = useState(true);
+
+  const handleButton = () => {
+    setError(false);
+    const audio = new Audio(errorSound);
+    audio.play();
+  };
+
   return (
-    <Div>
-      <h1>ქვიზის დაწყება</h1>
-      <div>
-        <button>დიახ</button>
-        <button>დიახ</button>
-      </div>
-    </Div>
+    <div>
+      {error ? (
+        <Div>
+          <h1>ქვიზის დაწყება</h1>
+          <div>
+            {["დიახ", "დიახ"].map((item, index) => (
+              <button key={index} onClick={handleButton}>
+                {item}
+              </button>
+            ))}
+          </div>
+        </Div>
+      ) : (
+        <ErrorDiv>
+          <h2>შეცდომა გეგონა ?</h2>
+          <button onClick={() => console.log("sad")}>კი</button>
+        </ErrorDiv>
+      )}
+    </div>
   );
 };
 
@@ -40,5 +63,35 @@ const Div = styled.div`
   @media (min-width: 1100px) {
     background: url(${windows11}) no-repeat;
     padding: 24.5rem 25.8rem;
+    button {
+      cursor: pointer;
+      transition: background-color 0.5s ease-in-out, font-size 0.5s ease-in-out;
+    }
+    button:hover {
+      background-color: #292929;
+      font-size: 3.5rem;
+    }
+  }
+`;
+
+const ErrorDiv = styled.div`
+  background: url(${error}) no-repeat;
+  padding: 11.8rem 3rem 3.5rem 8rem;
+  h2 {
+    font-size: 2.4rem;
+  }
+  button {
+    font-size: 2.8rem;
+    background: transparent;
+    border: 0;
+    margin-top: 2.6rem;
+    padding: 0 3rem;
+    margin-left: -2.5rem;
+  }
+
+  @media (min-width: 1100px) {
+    button {
+      cursor: pointer;
+    }
   }
 `;
